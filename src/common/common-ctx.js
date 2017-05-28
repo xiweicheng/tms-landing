edc90147861328eb5af4b1bb40653cfc533d1310
weicheng xi <weicheng.xi@newtouch.cn>
2017-05-28 22:12:33 +0800

    init commit

---
+++ src/common/common-ctx.js
@@ -0,0 +1,13 @@
+window.nsCtx = {
+    memberAll: {
+        username: 'all',
+        enabled: true,
+        mails: '',
+        name: '全员'
+    },
+    isAt: true, // is channel or user chat
+    chatTo: null, // chat to channel name or username
+    chatId: null, // chat to channel name or @username 
+    isSuper: false,
+    isAdmin: false,
+}
