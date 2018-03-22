bdaf93250d2a78f72c61e429a247d1fc7b5cdc85
xiweicheng <xiweicheng@yeah.net>
2018-03-22 21:49:20 +0800

    #IHL0J 首页博文列表提供检索功能

--- src/resources/elements/em-blog-list.js
+++ src/resources/elements/em-blog-list.js
@@ -5,12 +5,23 @@ export class EmBlogList {
 
     @bindable id;
 
+    search = '';
+
     page = 0;
     size = 10;
     blogs = [];
 
+    constructor() {
+        this.doSearch = _.debounce(() => {
+            this.blogs = [];
+            this.page = 0;
+            this._listBlogs();
+        }, 500);
+    }
+
     _listBlogs() {
-        this.ajax = $.get('/free/home/blog/list', {
+        this.ajax = $.get('/free/home/blog/page/search', {
+            search: this.search,
             size: this.size,
             page: this.page
         }, (data) => {
@@ -20,11 +31,31 @@ export class EmBlogList {
     }
 
     moreHandler() {
-    	this.page++;
-    	this._listBlogs();
+        this.page++;
+        this._listBlogs();
     }
 
     bind(bindingCtx, overrideCtx) {
-    	this._listBlogs();
+        this._listBlogs();
+    }
+
+    keyupHandler(event) {
+        this.doSearch();
+    }
+
+    searchFocusHandler() {
+        $(this.searchRemoveRef).show();
+    }
+
+    searchBlurHandler() {
+        if (!this.search) {
+            $(this.searchRemoveRef).hide();
+        }
+    }
+
+    clearSearchHandler() {
+        this.search = '';
+        $(this.searchInputRef).focus();
+        this.doSearch();
     }
 }
