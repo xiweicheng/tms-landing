87a33c3cfb8565657ecb068efc05b1b3593c48ac
xiweicheng <xiweicheng@yeah.net>
2020-01-01 09:27:07 +0800

    markdown上传图片展示自定宽高单位解析问题 bug

--- src/init/config.js
+++ src/init/config.js
@@ -195,7 +195,7 @@ export class Config {
                     if (_.isNumber(+width) && (+width <= 100)) {
                         style.width = width + '%';
                     } else {
-                        style.width = width + (_.isNumber(+width) ? 'px' : '');
+                        style.width = width + (!_.isNaN(+width) && _.isNumber(+width) ? 'px' : '');
                     }
                 }
 
@@ -205,7 +205,7 @@ export class Config {
                     if (_.isNumber(+height) && +height <= 100) {
                         style.height = height + '%';
                     } else {
-                        style.height = height + (_.isNumber(+height) ? 'px' : '');
+                        style.height = height + (!_.isNaN(+height) && _.isNumber(+height) ? 'px' : '');
                     }
                 }
 
