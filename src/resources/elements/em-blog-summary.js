d98f0f163ffb3aee768859ea13b8844c7ef2c3f0
寒烟濡雨 <1484643646@qq.com>
2020-12-26 22:28:48 +0800

    !2 优化一些首页展示 * 参考简书排版，取消预览框标题样式 * '修复重复加载问题，优化部分排版' * 将blog图片移动到左侧，保证排版一致。修改了一些样式。增加滚动加载功能，到指定的recordCount，再让用户手动加载。

--- src/resources/elements/em-blog-summary.js
+++ src/resources/elements/em-blog-summary.js
@@ -1,4 +1,5 @@
 import { bindable, containerless } from 'aurelia-framework';
+import {observable} from "aurelia-binding";
 
 @containerless
 export class EmBlogSummary {
@@ -6,8 +7,27 @@ export class EmBlogSummary {
     @bindable sid;
 
     page = 0;
-    size = 3;
+    size = 7;
     blogs = [];
+    loading = false;
+    maxLoad = 3;//最大滚动加载次数
+
+    constructor() {
+        let tmpThis = this;//下文因function this对象变化
+
+        $(window).scroll(function() { //滚动加载
+            let scrollHeight = $(document).height();//document的滚动高度
+            let scrollTop = $(document).scrollTop() || $(this).scrollTop();//已滚动高度
+            let windowHeight = $(this).height(); //可视区高度
+            let overHeight = 200;//距底部开始加载的距离
+            // 简单判断，避免滚动过快时多次加载
+            // 理论上应该是页面渲染完再修改，aurelia无此回调接口，观察者属性亦不生效，退而求其次只判断数据加载
+            if (!tmpThis.loading && scrollTop + windowHeight + overHeight >= scrollHeight && tmpThis.page < tmpThis.maxLoad) {
+                tmpThis.page++;
+                tmpThis._getBlogs();
+            }
+        });
+    }
 
     bind() {
         this._getBlogs();
@@ -18,12 +38,14 @@ export class EmBlogSummary {
         if (this.sid) {
             url = `/free/space/home/${this.sid}/blogs`;
         }
+        this.loading = true;
         this.ajax = $.get(url, {
             page: this.page,
             size: this.size,
         }, (data) => {
             this.blogPage = data.data;
             this.blogs.push(...data.data.content);
+            this.loading = false;
         });
     }
 
