e4edfc03e9142289836b8e97df07eb5b60898e47
xiweicheng <xiweicheng@yeah.net>
2021-01-24 12:11:48 +0800

    博文支持限制附件文件下载功能

--- src/resources/elements/em-blog-share-ppt-content.js
+++ src/resources/elements/em-blog-share-ppt-content.js
@@ -1,4 +1,7 @@
-import { bindable, containerless } from 'aurelia-framework';
+import {
+    bindable,
+    containerless
+} from 'aurelia-framework';
 import {
     default as Clipboard
 } from 'clipboard';
@@ -13,37 +16,69 @@ export class EmBlogSharePptContent {
 
     attached() {
         new Clipboard('.em-blog-share-ppt-content .tms-clipboard')
-            .on('success', function(e) {
+            .on('success', function (e) {
                 toastr.success('复制到剪贴板成功!');
-            }).on('error', function(e) {
+            }).on('error', function (e) {
                 toastr.error('复制到剪贴板失败!');
             });
 
-        $('.em-blog-share-ppt-content').on('click', 'code[data-code]', function(event) {
+        this.codeClHandler = function (event) {
             if (event.ctrlKey) {
                 event.stopImmediatePropagation();
                 event.preventDefault();
                 clipboard.copy($(event.currentTarget).attr('data-code')).then(
-                    () => { toastr.success('复制到剪贴板成功!'); },
-                    (err) => { toastr.error('复制到剪贴板失败!'); }
+                    () => {
+                        toastr.success('复制到剪贴板成功!');
+                    },
+                    (err) => {
+                        toastr.error('复制到剪贴板失败!');
+                    }
                 );
             }
-        });
+        };
 
-        $('.em-blog-share-ppt-content').on('click', '.pre-code-wrapper', function(event) {
+        this.preCodeClHandler = function (event) {
             if (event.ctrlKey) {
                 event.stopImmediatePropagation();
                 event.preventDefault();
                 clipboard.copy($(event.currentTarget).find('i[data-clipboard-text]').attr('data-clipboard-text')).then(
-                    () => { toastr.success('复制到剪贴板成功!'); },
-                    (err) => { toastr.error('复制到剪贴板失败!'); }
+                    () => {
+                        toastr.success('复制到剪贴板成功!');
+                    },
+                    (err) => {
+                        toastr.error('复制到剪贴板失败!');
+                    }
                 );
             }
-        });
+        };
+
+        this.fileDownloadLinkClickHandler = (event) => {
+
+            if (this.blog.fileReadonly) {
+                toastr.error('附件文件下载权限不足！');
+                event.preventDefault();
+            }
+        };
+
+        $('.em-blog-share-ppt-content').on('click', 'code[data-code]', this.codeClHandler);
+
+        $('.em-blog-share-ppt-content').on('click', '.pre-code-wrapper', this.preCodeClHandler);
+
+        // file online preview
+        $('.em-blog-share-ppt-content').on('click', '.tms-blog-content.markdown-body a[href*="admin/file/download/"],a.tms-file-download-item', this.fileDownloadLinkClickHandler);
 
         if (_.includes(wurl(), '?read')) { // read
             $('.ppt-dimmer').dimmer('show');
         }
+    }
+
+    detached() {
+
+        $('.em-blog-share-ppt-content').off('click', 'code[data-code]', this.codeClHandler);
+        $('.em-blog-share-ppt-content').off('click', '.pre-code-wrapper', this.preCodeClHandler);
+        // file online preview
+        $('.em-blog-share-ppt-content').off('click', '.tms-blog-content.markdown-body a[href*="admin/file/download/"],a.tms-file-download-item', this.fileDownloadLinkClickHandler);
+
     }
 
 }
